01220c3d8dd14c1f144541c071607b022f2b84e5
Rohit K <rohitrkaryappa@yahoo.com>
2022-07-12 23:32:32 +0400

    Added Article list model class & Constant file

---
+++ Article Viewer/AppConstants.swift
@@ -0,0 +1,20 @@
+//
+//  AppConstants.swift
+//  Article Viewer
+//
+//  Created by Rohit Karyappa on 12/07/2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct AppVariables {
+        static let apiKey = "ZfGywKVlnaa758DsWgVvWcF1BmREX5sb"
+
+    }
+    
+    struct API {
+        static let getArticleListURLString = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key="
+    }
+}
---
+++ Article Viewer/ArticleListModel.swift
@@ -0,0 +1,50 @@
+//
+//  ArticleListModel.swift
+//  Article Viewer
+//
+//  Created by Rohit Karyappa on 12/07/2022.
+//
+
+import Foundation
+
+struct ArticleListModel: Decodable {
+    var status: String
+    var num_results: Int
+    
+    var results: [ArticleDeatils]
+    
+    struct ArticleDeatils: Decodable {
+        var url: String
+        var source: String
+        var publishedDate: String
+        var title: String
+        var abstract: String
+        var media: [MediaDetails]
+        
+        enum CodingKeys: String, CodingKey {
+            case url
+            case source
+            case publishedDate = "published_date"
+            case title
+            case abstract
+            case media
+        }
+        
+        
+        struct MediaDetails: Decodable {
+            var type: String
+            var copyright: String
+            var mediaMetadata: [MediaItemDetails]?
+            
+            enum CodingKeys: String, CodingKey {
+                case type
+                case copyright
+                case mediaMetadata = "media-metadata"
+            }
+         
+            struct MediaItemDetails: Codable {
+                var url: String
+            }
+        }
+    }
+}
