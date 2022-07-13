049fa26c4164433254fd7be8db060c28fcd212e6
Rohit K <rohitrkaryappa@yahoo.com>
2022-07-13 16:47:40 +0400

    Pods changes

---
+++ Pods/Target Support Files/Pods-Article Viewer/Pods-Article Viewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Article_Viewer : NSObject
+@end
+@implementation PodsDummy_Pods_Article_Viewer
+@end
---
+++ Pods/Target Support Files/Pods-Article ViewerTests/Pods-Article ViewerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Article_ViewerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Article_ViewerTests
+@end
