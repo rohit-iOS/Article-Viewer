9d795c6b7abd8da8f0aed203d72c704a2b8685dc
Rohit K <rohitrkaryappa@yahoo.com>
2022-07-13 17:10:26 +0400

    Image cashing implementation done

--- Article Viewer/Common.swift
+++ Article Viewer/Common.swift
@@ -14,17 +14,32 @@ enum TimeFrame: Int {
     case month = 30
 }
 
+
+class ImageStore: NSObject {
+    static let imageCache = NSCache<NSString, UIImage>()
+}
+
 extension UIImageView {
-    func loadFrom(urlString: String) {
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        
-        DispatchQueue.main.async { [weak self] in
-            if let imageData = try? Data(contentsOf: url) {
-                if let loadedImage = UIImage(data: imageData) {
-                    self?.image = loadedImage
+    func loadFrom(urlString: String?) {
+        DispatchQueue.global().async { [weak self] in
+            guard let stringURL = urlString, let url = URL(string: stringURL) else {
+                return
+            }
+            func setImage(image:UIImage?) {
+                DispatchQueue.main.async {
+                    self?.image = image
                 }
+            }
+            let urlToString = url.absoluteString as NSString
+            if let cachedImage = ImageStore.imageCache.object(forKey: urlToString) {
+                setImage(image: cachedImage)
+            } else if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    ImageStore.imageCache.setObject(image, forKey: urlToString)
+                    setImage(image: image)
+                }
+            }else {
+                setImage(image: nil)
             }
         }
     }
